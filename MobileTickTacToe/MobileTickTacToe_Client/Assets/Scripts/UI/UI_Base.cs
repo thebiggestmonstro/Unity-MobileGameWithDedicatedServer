188b2991edcb2cc389eb6c862a4bcb352e2e469b
thebiggestmonstro <monstroowner@gmail.com>
2025-11-11 20:09:01 +0900

    Setup Login UI

--- MobileTickTacToe/MobileTickTacToe_Client/Assets/Scripts/UI/UI_Base.cs
+++ MobileTickTacToe/MobileTickTacToe_Client/Assets/Scripts/UI/UI_Base.cs
@@ -1,6 +1,9 @@
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Base : MonoBehaviour
@@ -10,18 +13,32 @@ public class UI_Base : MonoBehaviour
     protected void Bind<T>(Type type) where T : UnityEngine.Object
     {
         string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
+        UnityEngine.Object[] newObjects = new UnityEngine.Object[names.Length];
 
         for (int i = 0; i < names.Length; i++)
         {
             if (typeof(T) == typeof(GameObject))
-                objects[i] = Util.FindChild(gameObject, names[i], true);
+                newObjects[i] = Util.FindChild(gameObject, names[i], true);
             else
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+                newObjects[i] = Util.FindChild<T>(gameObject, names[i], true);
 
-            if (objects[i] == null)
+            if (newObjects[i] == null)
                 Debug.Log($"Failed to bind({names[i]})");
+        }
+
+        // 기존에 존재하는 Key의 인스턴스들을 바인드하는 경우 
+        if (_objects.ContainsKey(typeof(T)))
+        {
+            UnityEngine.Object[] existingObjects = _objects[typeof(T)];
+            UnityEngine.Object[] combined = new UnityEngine.Object[existingObjects.Length + newObjects.Length];
+            existingObjects.CopyTo(combined, 0);
+            newObjects.CopyTo(combined, existingObjects.Length);
+            _objects[typeof(T)] = combined;
+        }
+        // 새로운 Key의 인스턴스들을 바인드하는 경우
+        else
+        {
+            _objects.Add(typeof(T), newObjects);
         }
     }
 
@@ -35,4 +52,24 @@ public class UI_Base : MonoBehaviour
     }
 
     protected GameObject GetObject(int idx) { return Get<GameObject>(idx); }
+
+    protected void GenerateEnumsSerialNumber<T>(Dictionary<string, int> dict) where T : UI_Base
+    {
+        var enums = typeof(T).GetNestedTypes().Where(t => t.IsEnum);
+
+        int counter = 0;
+
+        foreach (var e in enums)
+        {
+            foreach (var name in Enum.GetNames(e))
+            {
+                dict[$"{e.Name}.{name}"] = counter++;
+            }
+        }
+    }
+
+    protected string GetEnumFullName<T>(T value) where T : Enum
+    {
+        return $"{typeof(T).Name}.{value}";
+    }
 }
--- MobileTickTacToe/MobileTickTacToe_Client/Assets/Scripts/UI/UI_Login.cs
+++ MobileTickTacToe/MobileTickTacToe_Client/Assets/Scripts/UI/UI_Login.cs
@@ -1,30 +1,109 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Login : UI_Base
 {
-    enum GameObjects_Btn
+    Dictionary<string, int> enumNumbers = new Dictionary<string, int>();
+
+    private string _userName = string.Empty;
+    private string _password = string.Empty;
+
+    [SerializeField]
+    int maxUserNameLength = 10;
+    [SerializeField]
+    int maxPasswordLength = 10;
+
+    public enum GameObjects_Btn
     {
-        Btn_Connect,
-        Btn_Send
+        Btn_Login,
+        Btn_Quit,
+    }
+
+    public enum GameObjects_Input
+    {
+        Input_UserName,
+        Input_Password,
+    }
+
+    public enum GameObjects_Text
+    {
+        Txt_InputUserNameError,
+        Txt_InputPasswordError,
+        Txt_LoginError,
+    }
+
+    private void Awake()
+    {
+        GenerateEnumsSerialNumber<UI_Login>(enumNumbers);
+
+        Bind<GameObject>(typeof(GameObjects_Btn));
+        Bind<GameObject>(typeof(GameObjects_Input));
+        Bind<GameObject>(typeof(GameObjects_Text));
     }
 
     private void Start()
     {
-        Bind<GameObject>(typeof(GameObjects_Btn));
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Text.Txt_InputUserNameError)]).SetActive(false);
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Text.Txt_InputPasswordError)]).SetActive(false);
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Text.Txt_LoginError)]).SetActive(false);
 
-        GetObject((int)GameObjects_Btn.Btn_Connect).GetOrAddComponent<Button>().onClick.AddListener(Connect);
-        GetObject((int)GameObjects_Btn.Btn_Send).GetOrAddComponent<Button>().onClick.AddListener(Send);
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Btn.Btn_Login)]).GetOrAddComponent<Button>().onClick.AddListener(Login);
+
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Input.Input_UserName)]).GetOrAddComponent<TMP_InputField>().onValueChanged.AddListener(UpdateUserName);
+        GetObject(enumNumbers[GetEnumFullName(GameObjects_Input.Input_Password)]).GetOrAddComponent<TMP_InputField>().onValueChanged.AddListener(UpdatePassword);
     }
 
-    private void Connect()
+    private void Login()
     {
-        NetworkClient.Instance.Connect();
     }
 
-    private void Send()
+    private void UpdateUserName(string value)
     {
-        NetworkClient.Instance.SendServer("Hello From Client!!");
+        _userName = value;
+        ValidateAndUpdateUI();
+    }
+
+    private void UpdatePassword(string value) 
+    {
+        _password = value;
+        ValidateAndUpdateUI();
+    }
+
+    private void ValidateAndUpdateUI()
+    {
+        var userNameRegex = Regex.Match(_userName, "^[a-zA-Z0-9]+$");
+
+        var interactable =  
+            !string.IsNullOrWhiteSpace(_userName) && 
+            !(string.IsNullOrWhiteSpace(_password)) && 
+            (_userName.Length <= maxUserNameLength && _password.Length <= maxPasswordLength) &&
+            userNameRegex.Success;
+
+        EnableLoggingButton(interactable);
+
+        if (_userName != null)
+        {
+            var userNameTooLong = _userName.Length > maxUserNameLength || !userNameRegex.Success;
+            GetObject(enumNumbers[GetEnumFullName(GameObjects_Text.Txt_InputUserNameError)]).SetActive(userNameTooLong);
+        }
+
+        if (_password != null)
+        {
+            var passwordTooLong = _password.Length > maxPasswordLength;
+            GetObject(enumNumbers[GetEnumFullName(GameObjects_Text.Txt_InputPasswordError)]).SetActive(passwordTooLong);
+        }
+    }
+
+    private void EnableLoggingButton(bool interactable)
+    {
+        GameObject loginButton = GetObject(enumNumbers[GetEnumFullName(GameObjects_Btn.Btn_Login)]);
+
+        loginButton.GetOrAddComponent<Button>().interactable = interactable;
+        var color = loginButton.GetOrAddComponent<Button>().interactable ? Color.white : Color.grey;
+        loginButton.GetComponentInChildren<TextMeshProUGUI>().color = color;
     }
 }
